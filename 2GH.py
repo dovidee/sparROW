87f55f49a9567059d08c8b67fc8d2be1f8fef9e2
dovidee <138241775+dovidee@users.noreply.github.com>
2024-09-29 21:54:03 +0200

    Update 2GH.py

--- 2GH.py
+++ 2GH.py
@@ -6,7 +6,7 @@ from functools import reduce
 exc1 = pd.read_excel("oppgave2.xlsx")
 exc2 = pd.read_excel("oppgave2_rounded.xlsx") # Bruk denne
 
-def kommune_pie(kommune, exc) -> None:
+def kommune_pie(kommune : str, exc : dict) -> None:
     matched = exc['Sted'].str.fullmatch(kommune)
     chosen = exc.loc[matched[matched].index] # Boolean indexing til å finne kommune index (wtf?)
     to_flatten = chosen.values.tolist() # Velg tallene
@@ -31,7 +31,7 @@ def kommune_pie(kommune, exc) -> None:
     device_chart = alt.layer(pie, text) # Legg til teksten i radiusen utenfor
     device_chart.save(f"{kommune}.html") # Lagre chart i html
 
-def kommune_top10(exc) -> None:
+def kommune_top10(exc : dict) -> None:
     fixed = exc.replace(0, np.nan) # Fjern null
     fixed_again = fixed.dropna() # Fjern alle rader med ingen data
     fixed_again['mean'] = fixed_again.iloc[:, 1:].mean(axis=1) # Lag ny rad med gjennomsnitt
